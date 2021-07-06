2163ac982f6c5c3b2e0f75a4baf26d8eadd5cec3
Joram Wambugu <artisan254@gmail.com>
2021-07-06 15:47:31 +0300

    Setup
    Create the form for uploading photos to transform.

---
+++ src/components/navigation/BottomNavbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="appBottomMenu">
+    <router-link :to="{ name: 'home' }" class="item">
+      <div class="col">
+        <ion-icon name="cloud-upload-outline" class="md hydrated"></ion-icon>
+
+        <strong>Transform Photos</strong>
+      </div>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BottomNavbar",
+};
+</script>
+
+<style scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,111 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="appHeader">
+      <div class="pageTitle">Transform Photos</div>
+      <div class="right"></div>
+    </div>
+
+    <div id="appCapsule" class="mb-3">
+      <div class="section mt-2">
+        <div class="section-title">Choose Photo</div>
+        <div class="card">
+          <div class="card-body">
+            <div class="custom-file-upload" id="file-chooser">
+              <input type="file" id="photo-upload" accept=".png, .jpg, .jpeg" />
+              <label for="photo-upload">
+                <span>
+                  <strong>
+                    <ion-icon
+                      name="arrow-up-circle-outline"
+                      role="img"
+                      class="md hydrated"
+                      aria-label="arrow up circle outline"
+                    ></ion-icon>
+                    <i>Upload a Photo</i>
+                  </strong>
+                </span>
+              </label>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="section mt-2">
+        <div class="section-title">Select a Shape To Use</div>
+        <div class="card">
+          <div class="card-body p-0">
+            <div class="input-list">
+              <div class="form-check">
+                <input
+                  type="checkbox"
+                  class="form-check-input"
+                  id="customCheckd1"
+                />
+                <label class="form-check-label" for="customCheckd1">$ 50</label>
+              </div>
+              <div class="form-check">
+                <input
+                  type="checkbox"
+                  class="form-check-input"
+                  id="customCheckd2"
+                />
+                <label class="form-check-label" for="customCheckd2"
+                  >$ 100</label
+                >
+              </div>
+              <div class="form-check">
+                <input
+                  type="checkbox"
+                  class="form-check-input"
+                  id="customCheckd3"
+                />
+                <label class="form-check-label" for="customCheckd3"
+                  >$ 200</label
+                >
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="section mt-2">
+        <div class="section-title">Select Number of Shapes</div>
+        <div class="card">
+          <div class="card-body p-0">
+            <div class="input-list">
+              <div class="form-check">
+                <input
+                  type="checkbox"
+                  class="form-check-input"
+                  id="number-of-shapes"
+                />
+                <label class="form-check-label" for="number-of-shapes"
+                  >$ 50</label
+                >
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="section mt-2">
+        <button type="submit" class="btn btn-primary btn-block btn-lg">
+          Transform
+        </button>
+      </div>
+    </div>
+
+    <bottom-navbar></bottom-navbar>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import BottomNavbar from "../components/navigation/BottomNavbar";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  components: { BottomNavbar },
+};
 </script>
